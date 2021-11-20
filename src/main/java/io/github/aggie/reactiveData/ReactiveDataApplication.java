0d3a36be22980fce04f6c1ed7e73d0f0fd95e619
aggie-ch <aga.chorowiec@gmail.com>
2021-11-20 09:06:38 +0100

    Initial commit

---
+++ src/main/java/io/github/aggie/reactiveData/ReactiveDataApplication.java
@@ -0,0 +1,13 @@
+package io.github.aggie.reactiveData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveDataApplication.class, args);
+	}
+
+}
